467a65722a8a35a209db64d56b1ed38f5d834447
Shan Yang <shanyang@google.com>
2021-06-17 21:32:40 -0700

    Remove internal files and dependencies.

--- core/multi_modal_model.py
+++ core/multi_modal_model.py
@@ -11,11 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Abstract multi-modal model.
-
-This file defines a generic multi-modal model base class for multi-modal
-video understanding task: go/multi-modal-video-understanding
-"""
+"""Abstract multi-modal model."""
 import abc
 import google3
 
